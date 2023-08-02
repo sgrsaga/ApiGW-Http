f0c3002742adc597d7842d6f491e55320a8a4e99
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-08-02 09:16:52 +0530

    Api Gateway HTTP API

---
+++ hello-world/app.mjs
@@ -0,0 +1,56 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+
+ import AWS from 'aws-sdk';
+ //const AWS = require('aws-sdk')
+ 
+ AWS.config.update({
+      region: 'ap-south-1'
+  });
+ 
+ //const myProductsTable = AWS.DynamoDB.DocumentClient();
+ const productTableName = 'products';
+ const healthyPath = '/health';
+ const getProductsPath = '/products';
+ const addProductsPapth = '/addProd';
+ const updateProductPath = '/updateProd';
+ 
+ 
+ export const lambdaHandler = async (event, context) => {
+     try {
+         //console.log(event);
+         console.log(AWS.VERSION);
+         console.log(event);
+       
+         let response;
+         switch(true){
+             case event.routeKey === 'GET /health':
+                 console.log(healthyPath + ' -> Success')
+                 response = {
+                     "statusCode": 200,
+                     "headers": {
+                       "Content-Type": "application/json"
+                     },
+                     "body": "{\"message\":\"Hello, world! - Success - healthyPath\"}"
+                   };
+         }
+         return response;
+         
+ 
+     } catch (err) {
+         console.log('My Error : ' + err);
+         return err;
+     }
+ };
+ 
